0f7ca0aeeac14fff6dcaf448431cd69b22c60755
Jason Lin <jason.lin@exfo.com>
2025-08-06 15:54:56 +0800

    edit privca and tands pages

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Privacyus } from "./pages/Privacy_us";
 //import 'leaflet/dist/leaflet.css';
 import WhatsAppChat from "./components/WhatsAppChat";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
@@ -31,11 +31,11 @@ const router = createBrowserRouter([
         element: <Aboutus/>,
       }, */
       {
-        path: "/privacy",
+        path: "privacy",
         element: <Privacyus />,
       },
       {
-        path: "/termofservice",
+        path: "termofservice",
         element: <TandSus />,
       },
       /*       {
